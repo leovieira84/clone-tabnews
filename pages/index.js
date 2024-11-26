b0816f5d0ee308b0f6f042fd15bc47822408157a
Leonardo <81532565+leovieira84@users.noreply.github.com>
2024-11-26 23:08:45 +0000

    inclusão da imagem

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-    return <h1>Hasta la victoria siempre!</h1>
+    return (
+        <div>
+            <img src='https://cdn.dribbble.com/users/1502200/screenshots/4876220/media/7791d4273a9898e8d6c3d6aad0bbf582.gif' />
+            <h1>Hasta la victoria siempre!</h1>
+        </div>
+    )
 }
 
 export default Home;
